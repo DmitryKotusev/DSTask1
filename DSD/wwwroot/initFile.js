36f8842f19ede96e8e74cb7111be7d00d55103a0
DmitryKotusev <dssa-1966@yandex.ru>
2018-09-16 21:20:25 +0300

    Almost finished html page

---
+++ DSD/wwwroot/initFile.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    function makeApplyZone(number) {
+        var table = document.getElementsByClassName('table-fill')[0];
+        var applyZone = table.insertRow(number);
+        applyZone.innerHTML = 
+        `<th colspan=12 class="app">
+            <div class="applyEdit">
+                <button class="buttonApplyClass">Apply</button>
+            </div>
+        </th>`
+    }
+
+    function deleteApplyZone(number) {
+        document.getElementsByClassName('table-fill')[0].deleteRow(number);
+    }
+
+    window.makeApplyZone = makeApplyZone;
+    window.deleteApplyZone = deleteApplyZone;
+})();
