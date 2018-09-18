d10fa2d6756980094c3ece287530503e426502b0
DmitryKotusev <dssa-1966@yandex.ru>
2018-09-18 18:50:51 +0300

    Finished add and edit functions

--- DSD/wwwroot/functions.js
+++ DSD/wwwroot/functions.js
@@ -25,6 +25,21 @@ var isAdded = false;
         </th>`
     }
 
+    function makeRecoverZone(number) {
+        let table = document.getElementsByClassName('table-fill')[0];
+        let applyZone = table.insertRow(number);
+        applyZone.innerHTML =
+            `<th colspan=12 class="app">
+            <div class="applyAddEdit">
+                <button class="buttonRecoverClass">Back</button>
+            </div>
+        </th>`
+    }
+
+    function deleteApplyAddZone(number) {
+        document.getElementsByClassName('table-fill')[0].deleteRow(number);
+    }
+
     function deleteApplyZone(number) {
         document.getElementsByClassName('table-fill')[0].deleteRow(number);
     }
@@ -109,12 +124,26 @@ var isAdded = false;
 
             if (event.target.className === 'buttonAddClass') {
                 alert('add');
+                addEventFunc();
                 return;
             } else if (event.target.closest('.buttonAddClass') !== null) {
                 alert('add');
+                addEventFunc();
                 return;
             }
-        } else {
+
+            if (event.target.className === 'buttonRecoverClass') {
+                alert('applyAdd');
+
+                return;
+            } else if (event.target.closest('.buttonRecoverClass') !== null) {
+                alert('applyAdd');
+
+                return;
+            }
+        }
+
+        if (isEdited) {
             if (event.target.className === 'buttonApplyClass') {
                 alert('apply');
                 applyEditing(event.target.closest('tr'));
@@ -125,9 +154,30 @@ var isAdded = false;
                 return;
             }
         }
+
+        if (isAdded) {
+            if (event.target.className === 'buttonApplyBackClass') {
+                alert('applyBack');
+                cancelAdd();
+                return;
+            } else if (event.target.closest('.buttonApplyBackClass') !== null) {
+                alert('applyBack');
+                cancelAdd();
+                return;
+            }
+
+            if (event.target.className === 'buttonApplyAddClass') {
+                alert('applyAdd');
+                applyAdding(event.target.closest('tr'));
+                return;
+            } else if (event.target.closest('.buttonApplyAddClass') !== null) {
+                alert('applyAdd');
+                applyAdding(event.target.closest('tr'));
+                return;
+            }
+        }
     }
 
-    //Доделать
     function editEventFunc(tr) {
         isEdited = true;
         disableAllButtons();
@@ -144,12 +194,19 @@ var isAdded = false;
     function addEventFunc() {
         isAdded = true;
         disableAllButtons();
-        // Дописать
+        let emptyZone = buildNewEmptyLine();
+        makeEditable(emptyZone);
+        let table = document.getElementsByClassName('table-fill')[0];
+        makeApplyAddZone(table.rows.length);
+    }
+
+    function deleteNewEmptyLine(number) {
+        document.getElementsByClassName('table-fill')[0].deleteRow(number);
     }
 
     function buildNewEmptyLine() {
         let table = document.getElementsByClassName('table-fill')[0];
-        let applyZone = table.insertRow(table.length);
+        let applyZone = table.insertRow(table.rows.length);
         applyZone.innerHTML =
             `<tr id="1">
         <th class="text-left">
@@ -193,20 +250,137 @@ var isAdded = false;
             </button>
         </th>
     </tr>`;
+        return applyZone;
+    }
+
+    function makeDate(text) {
+        let arrD = text.split(".");
+        arrD[1] -= 1;
+        let d = new Date(arrD[2], arrD[1], arrD[0]);
+        return d;
     }
 
     function applyEditing(tr) {
         let rowIndex = tr.rowIndex;
         let table = document.getElementsByClassName('table-fill')[0];
-        makeNonEditable(tr.previousElementSibling);
-        table.deleteRow(rowIndex);
-        enableAllButtons();
-        if (isEdited) {
-            isEdited = false;
-        } else {
-            isAdded = false;
+        if (checkData(tr.previousElementSibling)) {
+            let divs = tr.previousElementSibling.getElementsByTagName('div');
+            // divs = [].slice.apply(divs);
+            let elemToEdit = toEdit.find((element) => {
+                return element.oldId === divs[0].textContent
+            });
+            if (!elemToEdit) {
+                toEdit.push(
+                    {
+                        oldId: tr.previousElementSibling.id,
+                        playerid: divs[0].textContent,
+                        jersey: parseInt(divs[1].textContent, 10),
+                        fname: divs[2].textContent,
+                        sname: divs[3].textContent,
+                        position: divs[4].textContent,
+                        birthday: makeDate(divs[5].textContent),
+                        weight: parseInt(divs[6].textContent, 10),
+                        height: parseInt(divs[7].textContent, 10),
+                        birthcity: divs[8].textContent,
+                        birthstate: divs[9].textContent
+                    }
+                );
+            } else {
+                elemToEdit.playerid = divs[0].textContent,
+                elemToEdit.jersey = parseInt(divs[1].textContent, 10),
+                elemToEdit.fname = divs[2].textContent,
+                elemToEdit.sname = divs[3].textContent,
+                elemToEdit.position = divs[4].textContent,
+                elemToEdit.birthday = makeDate(divs[5].textContent),
+                elemToEdit.weight = parseInt(divs[6].textContent, 10),
+                elemToEdit.height = parseInt(divs[7].textContent, 10),
+                elemToEdit.birthcity = divs[8].textContent,
+                elemToEdit.birthstate = divs[9].textContent
+            }
+            // Тут было приравнивание id
+            makeNonEditable(tr.previousElementSibling);
+            table.deleteRow(rowIndex);
+            enableAllButtons();
+            if (isEdited) {
+                isEdited = false;
+            } else {
+                isAdded = false;
+            }
         }
     }
+
+    function applyAdding(tr) {
+        let rowIndex = tr.rowIndex;
+        let table = document.getElementsByClassName('table-fill')[0];
+        if (checkData(tr.previousElementSibling)) {
+            let divs = tr.previousElementSibling.getElementsByTagName('div');
+            tr.previousElementSibling.id = divs[0].textContent;
+            toAdd.push(
+                {
+                    playerid: divs[0].textContent,
+                    jersey: parseInt(divs[1].textContent, 10),
+                    fname: divs[2].textContent,
+                    sname: divs[3].textContent,
+                    position: divs[4].textContent,
+                    birthday: makeDate(divs[5].textContent),
+                    weight: parseInt(divs[6].textContent, 10),
+                    height: parseInt(divs[7].textContent, 10),
+                    birthcity: divs[8].textContent,
+                    birthstate: divs[9].textContent
+                }
+            );
+            makeNonEditable(tr.previousElementSibling);
+            table.deleteRow(rowIndex);
+            // Занести в массив
+            enableAllButtons();
+            if (isEdited) {
+                isEdited = false;
+            } else {
+                isAdded = false;
+            }
+        }
+    }
+
+    function isDateValid(date) {
+        var arrD = date.split(".");
+        arrD[1] -= 1;
+        var d = new Date(arrD[2], arrD[1], arrD[0]);
+        if ((d.getFullYear() == arrD[2]) && (d.getMonth() == arrD[1]) && (d.getDate() == arrD[0])) {
+            return true;
+        } else {
+            alert('Введена некорректная дата, формат даты: dd.mm.yyyy.');
+            return false;
+        }
+    }
+
+    function checkData(tr) {
+        let divs = tr.getElementsByTagName('div');
+        if (divs[0].textContent !== '') {
+            if (document.getElementById(divs[0].textContent) !== null) {
+                if (document.getElementById(divs[0].textContent) !== tr) {
+                    alert('Некорректный id');
+                    return false;
+                }
+            }
+        } else {
+            alert('Id не должен быть пустым');
+            return false;
+        }
+        if (divs[5].textContent !== '') {
+            return isDateValid(divs[5].textContent);
+        }
+        return true;
+    }
+
+    function cancelAdd() {
+        isAdded = false;
+        let table = document.getElementsByClassName('table-fill')[0];
+        deleteApplyAddZone(table.rows.length - 1);
+        deleteNewEmptyLine(table.rows.length - 1);
+        enableAllButtons();
+    }
+
+
 
     window.makeApplyZone = makeApplyZone;
     window.deleteApplyZone = deleteApplyZone;
@@ -217,4 +391,6 @@ var isAdded = false;
     window.globalListenerFunction = globalListenerFunction;
     window.buildNewEmptyLine = buildNewEmptyLine;
     window.makeApplyAddZone = makeApplyAddZone;
+    window.applyEditing = applyEditing;
+    window.cancelAdd = cancelAdd;
 })();
