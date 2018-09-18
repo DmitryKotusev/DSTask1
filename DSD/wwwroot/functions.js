13ea1592a8b64bc9a942f7059e8eab0c85f5daec
DmitryKotusev <dssa-1966@yandex.ru>
2018-09-18 04:02:48 +0300

    More progress in js, still some work remaining

--- DSD/wwwroot/functions.js
+++ DSD/wwwroot/functions.js
@@ -1,4 +1,5 @@
-var isOccupied = false;
+var isEdited = false;
+var isAdded = false;
 
 (function () {
     function makeApplyZone(number) {
@@ -8,6 +9,18 @@ var isOccupied = false;
             `<th colspan=12 class="app">
             <div class="applyEdit">
                 <button class="buttonApplyClass">Apply</button>
+            </div>
+        </th>`
+    }
+
+    function makeApplyAddZone(number) {
+        let table = document.getElementsByClassName('table-fill')[0];
+        let applyZone = table.insertRow(number);
+        applyZone.innerHTML =
+            `<th colspan=12 class="app">
+            <div class="applyAddEdit">
+                <button class="buttonApplyBackClass">Back</button>
+                <button class="buttonApplyAddClass">Apply</button>
             </div>
         </th>`
     }
@@ -67,12 +80,14 @@ var isOccupied = false;
     }
 
     function globalListenerFunction(event) {
-        if (!isOccupied) {
+        if (!isEdited && !isAdded) {
             if (event.target.className === 'buttonEditClass') {
                 alert('edit');
+                editEventFunc(event.target.closest('tr'));
                 return;
             } else if (event.target.closest('.buttonEditClass') !== null) {
                 alert('edit');
+                editEventFunc(event.target.closest('tr'));
                 return;
             }
 
@@ -102,9 +117,11 @@ var isOccupied = false;
         } else {
             if (event.target.className === 'buttonApplyClass') {
                 alert('apply');
+                applyEditing(event.target.closest('tr'));
                 return;
             } else if (event.target.closest('.buttonApplyClass') !== null) {
                 alert('apply');
+                applyEditing(event.target.closest('tr'));
                 return;
             }
         }
@@ -112,9 +129,83 @@ var isOccupied = false;
 
     //Доделать
     function editEventFunc(tr) {
-        let rowIndex = tr.rowIndex;
+        isEdited = true;
+        disableAllButtons();
+        let rowIndex = tr.rowIndex + 1;
 
-        makeApplyZone(makeApplyZone);
+        makeApplyZone(rowIndex);
+        makeEditable(tr);
+    }
+
+    function deleteEventFunc(tr) {
+        // Дописать
+    }
+
+    function addEventFunc() {
+        isAdded = true;
+        disableAllButtons();
+        // Дописать
+    }
+
+    function buildNewEmptyLine() {
+        let table = document.getElementsByClassName('table-fill')[0];
+        let applyZone = table.insertRow(table.length);
+        applyZone.innerHTML =
+            `<tr id="1">
+        <th class="text-left">
+            <div contenteditable="true"></div>
+        </th>
+        <th class="text-left">
+            <div contenteditable="true"></div>
+        </th>
+        <th class="text-left">
+            <div contenteditable="true"></div>
+        </th>
+        <th class="text-left">
+            <div contenteditable="true"></div>
+        </th>
+        <th class="text-left">
+            <div contenteditable="true"></div>
+        </th>
+        <th class="text-left">
+            <div contenteditable="true"></div>
+        </th>
+        <th class="text-left">
+            <div contenteditable="true"></div>
+        </th>
+        <th class="text-left">
+            <div contenteditable="true"></div>
+        </th>
+        <th class="text-left">
+            <div contenteditable="true"></div>
+        </th>
+        <th class="text-left">
+            <div contenteditable="true"></div>
+        </th>
+        <th class="text-center">
+            <button type="button" class="buttonEditClass">
+                <img src="edit_write_icon.png" class="iconClass">
+            </button>
+        </th>
+        <th class="text-center">
+            <button type="button" class="buttonDeleteClass">
+                <img src="delete_write_icon.png" class="iconClass">
+            </button>
+        </th>
+    </tr>`;
+    }
+
+    function applyEditing(tr) {
+        let rowIndex = tr.rowIndex;
+        let table = document.getElementsByClassName('table-fill')[0];
+        makeNonEditable(tr.previousElementSibling);
+        table.deleteRow(rowIndex);
+        enableAllButtons();
+        if (isEdited) {
+            isEdited = false;
+        } else {
+            isAdded = false;
+        }
     }
 
     window.makeApplyZone = makeApplyZone;
@@ -124,4 +215,6 @@ var isOccupied = false;
     window.enableAllButtons = enableAllButtons;
     window.disableAllButtons = disableAllButtons;
     window.globalListenerFunction = globalListenerFunction;
+    window.buildNewEmptyLine = buildNewEmptyLine;
+    window.makeApplyAddZone = makeApplyAddZone;
 })();
